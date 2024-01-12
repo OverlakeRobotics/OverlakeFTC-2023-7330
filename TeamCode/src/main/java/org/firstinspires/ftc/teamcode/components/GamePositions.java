cbc726c4d9e794473dc3b9a2423a4615ac234ec0
dashhyphenminus <robertblendea@outlook.com>
2024-01-11 21:11:28 -0800

    Tuned all Blue Paths, should be pretty good now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/GamePositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/GamePositions.java
@@ -13,7 +13,7 @@ public class GamePositions {
 
     public static final Pose2d BLUE_START_POS_1 = new Pose2d(14.5, 61, Math.toRadians(-90));
 
-    public static final Pose2d BLUE_OBJECT_POS_1 = new Pose2d(31, 37, Math.toRadians(-135));
+    public static final Pose2d BLUE_OBJECT_POS_1 = new Pose2d(31, 35, Math.toRadians(-135));
     public static final Pose2d BLUE_OBJECT_POS_2_1 = new Pose2d(32, 44, Math.toRadians(-90));
     public static final Pose2d BLUE_OBJECT_POS_2_2 = new Pose2d(22, 24, Math.toRadians(-135));
 
@@ -21,8 +21,12 @@ public class GamePositions {
     public static final Pose2d BLUE_OBJECT_POS_3 = new Pose2d(8, 33, Math.toRadians(-150));
 
     public static final Pose2d BLUE_BACKDROP_LEFT = new Pose2d(49, 39.5, Math.toRadians(-180));
-    public static final Pose2d BLUE_BACKDROP_CENTER = new Pose2d(49, 32.5, Math.toRadians(-180));
-    public static final Pose2d BLUE_BACKDROP_RIGHT = new Pose2d(49, 24, Math.toRadians(-180));
+    public static final Pose2d BLUE_BACKDROP_CENTER = new Pose2d(49, 35, Math.toRadians(-180));
+    public static final Pose2d BLUE_BACKDROP_RIGHT = new Pose2d(49, 27.5, Math.toRadians(-180));
+
+    public static final Pose2d BLUE_BACKDROP_LEFT_F = new Pose2d(49, 39.5, Math.toRadians(-180));
+    public static final Pose2d BLUE_BACKDROP_CENTER_F = new Pose2d(49, 29, Math.toRadians(-180));
+    public static final Pose2d BLUE_BACKDROP_RIGHT_F = new Pose2d(49, 22, Math.toRadians(-180));
 
 
     public static final Pose2d BLUE_START_POS_2 = new Pose2d(-38, 61, Math.toRadians(-90));
@@ -33,9 +37,9 @@ public class GamePositions {
 //    public static final Pose2d BLUE_OBJECT_POS_4_2 = new Pose2d(-33, 37, Math.toRadians(-30));
 //    public static final Pose2d BLUE_OBJECT_POS_4_3 = new Pose2d(-40, 37, Math.toRadians(-30));
 
-    public static final Pose2d BLUE_WAYPOINT_1 = new Pose2d(-35, 12, Math.toRadians(180));
+    public static final Pose2d BLUE_WAYPOINT_1 = new Pose2d(-35, 10, Math.toRadians(180));
 //    public static final Pose2d BLUE_WAYPOINT_1_5 = new Pose2d(0, 0, Math.toRadians(0));
-    public static final Pose2d BLUE_WAYPOINT_2 = new Pose2d(37, 11, Math.toRadians(180));
+    public static final Pose2d BLUE_WAYPOINT_2 = new Pose2d(37, 10, Math.toRadians(180));
 
     public static final Pose2d BLUE_OBJECT_POS_5 = new Pose2d(-36, 11.5, Math.toRadians(65));
     public static final Pose2d BLUE_WAYPOINT_C = new Pose2d (0, 12, Math.toRadians(180));
